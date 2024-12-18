44fb7e40bb34bb841f423611d0f4ed6fdc71b39f
Vijaysplendor <vijaysplendor@gmail.com>
2024-12-18 19:59:00 +0000

    Added member.js

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  return {  // return an object
+    name: 'John Doe',
+    age: 30,
+    skills: ['HTML', 'CSS', 'JS'],
+    greet: function() {
+      console.log('Hello');
+    }
+  };            
